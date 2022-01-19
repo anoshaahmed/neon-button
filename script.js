7d0e14a2c217528704d220fbfae30e4656a34a0d
Dev Rathore <92583680+GitHub-dev12345@users.noreply.github.com>
2022-01-19 10:03:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// First Program JavaScript 
+
+let hoverbtn = document.querySelector('button');
+hoverbtn.addEventListener('click', showMsg);
+
+function showMsg(){
+    alert("Welcome to the CSS Hover Button");
+}
